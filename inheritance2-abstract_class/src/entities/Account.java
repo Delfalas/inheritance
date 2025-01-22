a9e46237116c1aeaff4bf50082f983ce7d5533a5
Vinicius <vinidantas07@Hotmail.com>
2025-01-21 23:41:10 -0300

    Add files via upload

---
+++ inheritance2-abstract_class/src/entities/Account.java
@@ -0,0 +1,46 @@
+package entities;
+
+public abstract class Account { //classe abstrata não pode ser instanciada
+	
+	private Integer number;
+	private String holder;
+	protected Double balance; //modificador de acesso protected  para permitir o uso do objeto na subclasse
+		
+	public Account() {
+	}
+	
+	public Account(Integer number, String holder, Double balance) {
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public void withdraw(double amount) {
+		balance -= amount + 5.0;
+	}
+
+	public void deposit(double amount) {
+		balance += amount;
+	}
+
+}
